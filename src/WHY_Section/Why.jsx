4a25edc333feb6aa99206b168af765bef95ae322
mayor-style <150626238+mayor-style@users.noreply.github.com>
2024-07-10 02:09:20 +0100

    Update Why.jsx

--- src/WHY_Section/Why.jsx
+++ src/WHY_Section/Why.jsx
@@ -13,8 +13,7 @@ const Why = () => {
   useEffect(()=>{
   const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
-     if ( entry.isIntersecting) { entry.target.classList.add('show-Why') }else {entry.target.classList.remove('show-Why');
-     }
+     if ( entry.isIntersecting) { entry.target.classList.add('show-Why') }
     });
   });
 
