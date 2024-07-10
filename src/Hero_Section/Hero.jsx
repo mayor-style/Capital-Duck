7c1a62be53e6f45e9b276b973199423d0a795f8d
mayor-style <150626238+mayor-style@users.noreply.github.com>
2024-07-10 02:25:22 +0100

    Update Hero.jsx

--- src/Hero_Section/Hero.jsx
+++ src/Hero_Section/Hero.jsx
@@ -18,7 +18,8 @@ const Hero = () => {
     <section>
         <div className="HeroSec ">
       <h1 className='hidden'><span className='specialColor'>Welcome</span> to <span className='duck'>CapitalDuck,</span> the ultimate <span className='specialColor'>platform</span> where your taps turn into real rewards!</h1>
-        <p className='hidden'>Dive into a fun and engaging experience where farming coins is not just a game, but a rewarding adventure. </p>
+        <p className='hidden'>Dive in now, and start tapping your way to financial freedom. Your journey to earning, learning, and growing begins here!
+        </p>
         <button className='hidden'>Launch on Telegram</button>
         </div>
     </section>
